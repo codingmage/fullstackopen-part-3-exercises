d0d1a422cc944eeb5583c6495c87f1ac1226ff0e
Jean Fellipe <jeanfellipesilva93@gmail.com>
2023-10-07 23:43:00 -0300

    add exercises 3.7, 3.8

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :info'))
+morgan.token('info', (req, res) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  } else {
+    return
+  }
+})
 
 let persons = [
     { 
