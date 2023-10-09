4a8ee7c43fd56ce59e992bfbf2429a4dbcfdee7b
Jean Fellipe <jeanfellipesilva93@gmail.com>
2023-10-09 17:39:57 -0300

    add edits for exercise 3.19

--- index.js
+++ index.js
@@ -127,9 +127,12 @@ app.post('/api/persons', (req, res, next) => {
     number: body.number,
   })
 
-  person.save().then(savedPerson => {
-    res.json(savedPerson)
-  })
+  person.save()
+    .then(savedPerson => {
+      res.json(savedPerson)
+    })
+    .catch(error => next(error))
+
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
@@ -144,7 +147,7 @@ app.put('/api/persons/:id', (req, res, next) => {
 
   const newNumber = body.number
 
-  Person.findByIdAndUpdate(req.params.id, { $set: { number: newNumber } }, { new: true } )
+  Person.findByIdAndUpdate(req.params.id, { $set: { number: newNumber } }, { new: true, runValidators: true, context: 'query' } )
     .then(updatedPerson => {
       res.json(updatedPerson)
       console.log('Number updated.')
@@ -164,6 +167,8 @@ const errorHandler = (error, req, res, next) => {
 
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   } 
 
   next(error)
