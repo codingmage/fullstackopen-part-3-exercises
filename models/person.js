cc963d0951c9ec365660455a3bdd0c07d9b1f211
Jean Fellipe <jeanfellipesilva93@gmail.com>
2023-10-09 18:20:19 -0300

    add exercises 3.19, 3.20

--- models/person.js
+++ models/person.js
@@ -20,7 +20,16 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d*$/.test(v)
+      },
+      /* message: 'Please input a valid phone number.' */
+    }
+  },
 })
 
 personSchema.set('toJSON', {
