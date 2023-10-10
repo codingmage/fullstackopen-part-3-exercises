1bd2a2d73af7d3f895cc43006f41836e0fdbe9ad
Jean Fellipe <jeanfellipesilva93@gmail.com>
2023-10-10 02:01:37 -0300

    add exercises 3.21, 3.22

--- index.js
+++ index.js
@@ -1,80 +1,80 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const Person = require('./models/person')
+require("dotenv").config()
+const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors")
+const Person = require("./models/person")
 
 const app = express()
 
-app.use(express.static('dist'))
+app.use(express.static("dist"))
 
 app.use(express.json())
 
 app.use(cors())
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :info'))
-morgan.token('info', (req, res) => {
-  if (req.method === 'POST') {
-    return JSON.stringify(req.body)
-  } else {
-    return
-  }
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :info"))
+morgan.token("info", (req, res) => {
+	if (req.method === "POST") {
+		return JSON.stringify(req.body)
+	} else {
+		return
+	}
 })
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+/* let persons = [
+	{
+		"id": 1,
+		"name": "Arto Hellas",
+		"number": "040-123456"
+	},
+	{
+		"id": 2,
+		"name": "Ada Lovelace",
+		"number": "39-44-5323523"
+	},
+	{
+		"id": 3,
+		"name": "Dan Abramov",
+		"number": "12-43-234345"
+	},
+	{
+		"id": 4,
+		"name": "Mary Poppendieck",
+		"number": "39-23-6423122"
+	}
 ]
-
-app.get('/', (req, res) => {
-    res.send('<h1>Test</h1>')
-  })
-
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
-  })
+ */
+app.get("/", (req, res) => {
+	res.send("<h1>Test</h1>")
 })
 
-app.get('/api/info', (req, res) => {
-  const currentTime = new Date()
-
-  Person.countDocuments({})
-    .then(count => {
-      res.send(`<p>Phonebook has info for ${count} people</p> <p>${currentTime}</p>`)
-    })
-  
-  /* res.send(`<p>Phonebook has info for ${numberOfPeople} people</p> <p>${currentTime}</p>`) */
+app.get("/api/persons", (request, response) => {
+	Person.find({}).then(persons => {
+		response.json(persons)
+	})
 })
 
-app.get('/api/persons/:id', (req, res, next) => {
-  Person.findById(req.params.id)
-    .then(person => {
-      if (person) {
-        res.json(person)
-      } else {
-        res.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+app.get("/api/info", (req, res) => {
+	const currentTime = new Date()
+
+	Person.countDocuments({})
+		.then(count => {
+			res.send(`<p>Phonebook has info for ${count} people</p> <p>${currentTime}</p>`)
+		})
+
+	/* res.send(`<p>Phonebook has info for ${numberOfPeople} people</p> <p>${currentTime}</p>`) */
+})
+
+app.get("/api/persons/:id", (req, res, next) => {
+	Person.findById(req.params.id)
+		.then(person => {
+			if (person) {
+				res.json(person)
+			} else {
+				res.status(404).end()
+			}
+		})
+		.catch(error => next(error))
 })
 /*   const id = Number(req.params.id)
   const person = persons.find(person => person.id === id)
@@ -85,34 +85,34 @@ app.get('/api/persons/:id', (req, res, next) => {
     res.status(404).end()
   } */
 
-app.delete('/api/persons/:id', (req, res, next) => {
-  Person.findByIdAndRemove(req.params.id)
-    .then(result => {
-      res.status(204).end()
-    })
-    .catch(error => next(error))
+app.delete("/api/persons/:id", (req, res, next) => {
+	Person.findByIdAndRemove(req.params.id)
+		.then(result => {
+			res.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
 /* const generateId = () => {
   return Math.floor(Math.random() * 100000)
 } */
 
-app.post('/api/persons', (req, res, next) => {
-  const body = req.body
+app.post("/api/persons", (req, res, next) => {
+	const body = req.body
 
-  if (!body.name) {
-    return res.status(400).json({
-      error: 'name is missing'
-    })
-  }
+	if (!body.name) {
+		return res.status(400).json({
+			error: "name is missing"
+		})
+	}
 
-  if (!body.number) {
-    return res.status(400).json({
-      error: 'number is missing'
-    })
-  }
+	if (!body.number) {
+		return res.status(400).json({
+			error: "number is missing"
+		})
+	}
 
-/*   const alreadyExists = Person.findOne({ name: body.name })
+	/*   const alreadyExists = Person.findOne({ name: body.name })
 
   if(alreadyExists) {
     Person.findOne({ name: body.name })
@@ -122,56 +122,56 @@ app.post('/api/persons', (req, res, next) => {
     console.log('Person already in the phonebook.')
   }  */
 
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-  })
+	const person = new Person({
+		name: body.name,
+		number: body.number,
+	})
 
-  person.save()
-    .then(savedPerson => {
-      res.json(savedPerson)
-    })
-    .catch(error => next(error))
+	person.save()
+		.then(savedPerson => {
+			res.json(savedPerson)
+		})
+		.catch(error => next(error))
 
 })
 
-app.put('/api/persons/:id', (req, res, next) => {
-  const body = req.body
+app.put("/api/persons/:id", (req, res, next) => {
+	const body = req.body
 
-/*   if I wanted to allow name change as well:
+	/*   if I wanted to allow name change as well:
 
   const person = {
     name: body.name,
     number: body.number,
   } */
 
-  const newNumber = body.number
+	const newNumber = body.number
 
-  Person.findByIdAndUpdate(req.params.id, { $set: { number: newNumber } }, { new: true, runValidators: true, context: 'query' } )
-    .then(updatedPerson => {
-      res.json(updatedPerson)
-      console.log('Number updated.')
-    })
-    .catch(error => next(error))
-  
+	Person.findByIdAndUpdate(req.params.id, { $set: { number: newNumber } }, { new: true, runValidators: true, context: "query" } )
+		.then(updatedPerson => {
+			res.json(updatedPerson)
+			console.log("Number updated.")
+		})
+		.catch(error => next(error))
+
 })
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'unknown endpoint'})
+	res.status(404).send({ error: "unknown endpoint" })
 }
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
-  console.error(error.message)
+	console.error(error.message)
 
-  if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return res.status(400).json({ error: error.message })
-  } 
+	if (error.name === "CastError") {
+		return res.status(400).send({ error: "malformatted id" })
+	} else if (error.name === "ValidationError") {
+		return res.status(400).json({ error: error.message })
+	}
 
-  next(error)
+	next(error)
 }
 // This one being last middleware is intentional
 app.use(errorHandler)
@@ -179,5 +179,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
